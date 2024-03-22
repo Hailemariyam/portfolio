961045307ead2c38b4405099dcc28b7220b15c80
Hailemariyam <haile0938048261kebede@gmail.com>
2024-03-22 05:03:42 +0300

    first commit

--- src/pages/home/Footer.jsx
+++ src/pages/home/Footer.jsx
@@ -147,7 +147,7 @@ function Footer() {
             </li>
             <li>
               <a
-                href="https://www.linkedin.com/"
+                href="https://www.linkedin.com/in/hailat122129?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BU2bLptNAREig3te6sV2Mag%3D%3D"
                 className="navbar--content"
                 target="_blank"
                 rel="noreferrer"
@@ -173,7 +173,7 @@ function Footer() {
       </div>
       <hr className="divider" />
       <div className="footer--content--container">
-        <p className="footer--content">Made with 💖 by John</p>
+        <p className="footer--content">Developed by Hailemariyam</p>
         <div className="footer--social--icon">
           <ul>
             <li>
